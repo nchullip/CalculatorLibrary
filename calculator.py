1c9ea878366d6408ed3c009703e94ead454b3bc5
Nikhil <nikhilsc20@gmail.com>
2019-07-03 23:00:55 -0400

    Added Unit Tests

--- calculator.py
+++ calculator.py
@@ -4,9 +4,10 @@
 #
 ######
 
+
 def add(first_term, second_term):
-	return first_term + second_term
+    return first_term + second_term
 
 
 def subtract(first_term, second_term):
-	return first_term - second_term
+    return first_term - second_term
